b01cf67f45608f835922c9ace9957e9c6f9fedd5
John Daugherty <johndaugherty1986@gmail.com>
2017-08-12 01:18:29 -0500

    lexical this

--- app/js/index.js
+++ app/js/index.js
@@ -5,23 +5,15 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
 import Animal from "./Animal";
-logTitle('Arrow Functions');
+logTitle('Lexical this');
 /* coding examples */
-const hello = () => {
-  const es6 = 'ES6';
-  return `Hello ${es6}`;
-};
-
-const powers = [1,2,3,4,5].map((number, index) => Math.pow(number, index));
-
-
-const add = (n1, n2) =>  n1 + n2;
-
-
-const milesToKm = miles => miles * 1.60934;
-
-
-log(hello());
-log(powers);
-log(add(100,100));
-log(milesToKm(100));
+const person = {
+  name: "John",
+  cars: ['bike', 'camero'],
+  toString: function() {
+    this.cars.forEach((car) => {
+      log(`${this.name} has ${car}`);
+    });
+  }
+}
+person.toString();
