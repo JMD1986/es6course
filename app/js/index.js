34fc696020ef728f725f7552ace447c0e84a207a
John Daugherty <johndaugherty1986@gmail.com>
2017-08-11 22:49:40 -0500

    first commit

--- app/js/index.js
+++ app/js/index.js
@@ -3,6 +3,12 @@ require('styles/main.scss');
 /* js */
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
+import * as Math from "./Math";
 /* your imports */
-logTitle('Title');
+logTitle('Named Exports / imports');
 /* coding examples */
+log(Math.add(2,3));
+log(Math.subtract(2,3));
+log(Math.multiply(2,3));
+log(Math.divide(2,3));
+log(Math.pi);
