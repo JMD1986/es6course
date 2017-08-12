431d834cfcd3ec0c21c143d2865ba7eaa1705db0
John Daugherty <johndaugherty1986@gmail.com>
2017-08-11 23:15:38 -0500

    let keyword

--- app/js/index.js
+++ app/js/index.js
@@ -5,7 +5,11 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
 import Animal from "./Animal";
-logTitle('Named Exports / imports');
+logTitle('Let Keyword');
 /* coding examples */
-var animal = new Animal();
-log(animal.getClassType());
+/* here we are using the let keyword to make the variable local to the block behind the for loop */
+for(let i =0; i <10; i++) {
+  log(i);
+}
+/* notice that when you inspect i is not defined due to its scope! */
+log(i);
