270f2f1f31616d5c31aa0906ad99802bbeeeb28c
John Daugherty <johndaugherty1986@gmail.com>
2017-08-11 23:08:53 -0500

    continuing

--- app/js/index.js
+++ app/js/index.js
@@ -3,12 +3,9 @@ require('styles/main.scss');
 /* js */
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
-import * as Math from "./Math";
 /* your imports */
+import Animal from "./Animal";
 logTitle('Named Exports / imports');
 /* coding examples */
-log(Math.add(2,3));
-log(Math.subtract(2,3));
-log(Math.multiply(2,3));
-log(Math.divide(2,3));
-log(Math.pi);
+var animal = new Animal();
+log(animal.getClassType());
