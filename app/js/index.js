825a469985cf7b59d2be223cb8666384941407e0
John Daugherty <johndaugherty1986@gmail.com>
2017-08-12 17:49:52 -0500

    more generators

--- app/js/index.js
+++ app/js/index.js
@@ -5,18 +5,21 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('Generators');
 
-const getNumbers = function* () {
-  yield 1;
-  yield "hello";
-  yield true;
-  yield { name: 'Alex'};
-  return "i am done";
+const getNumbers = function* (numbers) {
+  for (var i = 0; i < numbers.length; i++) {
+    yield numbers[i];
+  }
 }
 
-const numbersGen = getNumbers();
+const getNumbersGen = getNumbers([1,2,3,4,5]);
 
-log(numbersGen.next().value);
-log(numbersGen.next().value);
-log(numbersGen.next().value);
-log(JSON.stringify(numbersGen.next().value));
-log(numbersGen.next().value);
+const interval = setInterval(()=> {
+  const next = getNumbersGen.next();
+  if(next.done) {
+    log("this generator is done");
+    clearInterval(interval);
+  }else{
+    const number = next.value;
+    log(number);
+  }
+}, 1000);
