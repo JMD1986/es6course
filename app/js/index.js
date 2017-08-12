b77717c77151a763a2f1cb5f769fb6f8b7bde499
John Daugherty <johndaugherty1986@gmail.com>
2017-08-12 16:04:20 -0500

    promises

--- app/js/index.js
+++ app/js/index.js
@@ -4,38 +4,21 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('ES6 Classes');
+logTitle('Promises');
 /* coding examples */
-class Animal {
-  constructor(name, age) {
-    log(`${name} is an animal and was created`);
-    this.name = name;
-    this.age = age;
-  }
-  eat() {
-    log(`${this.name} is eating`);
-  }
-  sleep(){
-    log(`${this.name} is eating`);
-  }
-  logAge(){
-    log(`${this.name} is ${this.age} years old!`);
-  }
-}
 
-class Dog extends Animal {
-  constructor(name, age, breed){
-    super(name, age)
-    this.breed = breed;
-  }
-  logBreed(){
-    log(`${this.name} is a ${this.breed}`)
-  }
-  logAgeFromDog(){
-    super.logAge();
-  }
-}
+const promise = new Promise((resolve, reject) =>{
+  setTimeout(()=>{
+    resolve("data back from the server");
+  }, 3000);
 
-const mike = new Dog("mike", 4, 'bulldog');
-mike.logBreed();
-mike.logAgeFromDog();
+  setTimeout(()=>{
+    reject("no data back from the server, there was an error");
+  }, 5000);
+});
+
+promise.then(response => {
+  log(response);
+}).catch(error => {
+  log(error);
+})
