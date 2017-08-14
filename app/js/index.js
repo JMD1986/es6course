f229da1f853d67f691287282f01820a01fff0ceb
John Daugherty <johndaugherty1986@gmail.com>
2017-08-13 22:45:56 -0500

    first react component

--- app/js/index.js
+++ app/js/index.js
@@ -3,23 +3,24 @@ require('styles/main.scss');
 /* js */
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Generators');
 
-const getNumbers = function* (numbers) {
-  for (var i = 0; i < numbers.length; i++) {
-    yield numbers[i];
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+logTitle('React & ES6');
+
+class MainComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1> Hello world </h1>
+    );
   }
 }
 
-const getNumbersGen = getNumbers([1,2,3,4,5]);
+const mountNode = document.getElementById('mountNode');
 
-const interval = setInterval(()=> {
-  const next = getNumbersGen.next();
-  if(next.done) {
-    log("this generator is done");
-    clearInterval(interval);
-  }else{
-    const number = next.value;
-    log(number);
-  }
-}, 1000);
+render(<MainComponent />, mountNode);
